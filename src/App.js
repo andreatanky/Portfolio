3bb06a3d307a2642670aeedb47b3faf411955531
Andrea Tan <andreaxtan611@gmail.com>
2021-01-06 19:14:35 +0800

    Add routes for navbar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Landing from './components/hero/landing';
 import Navbar from './components/my-navbar/navbar.js'
 import Projects from './pages/portfolio/projects';
 import Experience from "./pages/experience/experience";
+import Skills from "./pages/skills/skills";
+import About from "./pages/about/about";
+import Contact from "./pages/contact/contact";
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
       <Landing/>
       <Projects/>
       <Experience/>
+      <Skills/>
+      <About/>
+      <Contact/>
     </div>
   );
 }
