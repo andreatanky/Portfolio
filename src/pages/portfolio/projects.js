71435b704774d99fae3b0813ea3bb567e6f556a7
Andrea Tan <andreaxtan611@gmail.com>
2021-01-06 23:03:34 +0800

    Update projects row

--- src/pages/portfolio/projects.js
+++ src/pages/portfolio/projects.js
@@ -6,21 +6,60 @@ import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import {CardMedia, Container} from "@material-ui/core";
 import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import CardActions from "@material-ui/core/CardActions";
+import Button from "@material-ui/core/Button";
+import { ProjectItems } from './projectItems';
 
 const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
+        minWidth: 275,
     },
-    paper: {
-        padding: theme.spacing(2),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
-    },
-
     contain: {
         padding: theme.spacing(5),
-    }
+    },
+    grids: {
+        display: 'flex'
+    },
+    bullet: {
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)',
+    },
+    title: {
+        fontSize: 14,
+    },
+    pos: {
+        marginBottom: 12,
+    },
+    media: {
+        height: 320,
+    },
 }));
+
+// const useStyles = makeStyles({
+//     root: {
+//         minWidth: 275,
+//     },
+//     bullet: {
+//         display: 'inline-block',
+//         margin: '0 2px',
+//         transform: 'scale(0.8)',
+//     },
+//     title: {
+//         fontSize: 14,
+//     },
+//     pos: {
+//         marginBottom: 12,
+//     },
+//     media: {
+//         height: 320,
+//     },
+// });
+
 
 
 function Projects() {
@@ -30,8 +69,127 @@ function Projects() {
                 <Container className={classes.contain}>
                     <div className={classes.root}>
                         <h1>Projects</h1>
-                        <Grid container spacing = {3}>
-                            <Grid item xs={12} sm={6} lg={4}><CardItem/></Grid>
+                        <Grid className="classes.grids" container spacing = {3}>
+                            <Grid item xs={12} sm={6} lg={4}>
+                                {/*insert card here*/}
+
+                                <Card className={classes.root} style={{backgroundColor: "#ffff"}} variant="outlined">
+                                    <CardActionArea>
+                                        <CardMedia
+                                            className={classes.media}
+                                            image= {ProjectItems[0].imageUrl}
+                                            title= {ProjectItems[0].title}
+                                        />
+                                        <CardContent>
+                                            <Typography style={{color: "#ff006e", fontFamily:'Poppins', fontWeight: 'bold'}} className="title" gutterBottom variant="h5" component="h2">
+                                                {ProjectItems[0].title}
+                                            </Typography>
+                                            <Typography style={{color: "black", fontFamily:'Poppins'}} variant="body2" color="textSecondary" component="p">
+                                                {ProjectItems[0].description}
+                                            </Typography>
+                                        </CardContent>
+                                    </CardActionArea>
+                                    <CardActions>
+                                        <Button variant="contained" size="small" color="secondary">
+                                            Github
+                                        </Button>
+                                        <Button variant="contained" size="small" color="secondary">
+                                            Documentation
+                                        </Button>
+                                    </CardActions>
+                                </Card>
+                                {/*card ends here*/}
+                            </Grid>
+                            <Grid item xs={12} sm={6} lg={4}>
+                                {/*insert card here*/}
+
+                                <Card className={classes.root} style={{backgroundColor: "#ffff"}} variant="outlined">
+                                    <CardActionArea>
+                                        <CardMedia
+                                            className={classes.media}
+                                            image= {ProjectItems[1].imageUrl}
+                                            title= {ProjectItems[1].title}
+                                        />
+                                        <CardContent>
+                                            <Typography style={{color: "#ff006e", fontFamily:'Poppins', fontWeight: 'bold'}} className="title" gutterBottom variant="h5" component="h2">
+                                                {ProjectItems[1].title}
+                                            </Typography>
+                                            <Typography style={{color: "black", fontFamily:'Poppins'}} variant="body2" color="textSecondary" component="p">
+                                                {ProjectItems[1].description}
+                                            </Typography>
+                                        </CardContent>
+                                    </CardActionArea>
+                                    <CardActions>
+                                        <Button variant="contained" size="small" color="secondary">
+                                            Github
+                                        </Button>
+                                        <Button variant="contained" size="small" color="secondary">
+                                            Documentation
+                                        </Button>
+                                    </CardActions>
+                                </Card>
+                                {/*card ends here*/}
+                            </Grid>
+                            <Grid item xs={12} sm={6} lg={4}>
+                                {/*insert card here*/}
+
+                                <Card className={classes.root} style={{backgroundColor: "#ffff"}} variant="outlined">
+                                    <CardActionArea>
+                                        <CardMedia
+                                            className={classes.media}
+                                            image= {ProjectItems[2].imageUrl}
+                                            title= {ProjectItems[2].title}
+                                        />
+                                        <CardContent>
+                                            <Typography style={{color: "#ff006e", fontFamily:'Poppins', fontWeight: 'bold'}} className="title" gutterBottom variant="h5" component="h2">
+                                                {ProjectItems[2].title}
+                                            </Typography>
+                                            <Typography style={{color: "black", fontFamily:'Poppins'}} variant="body2" color="textSecondary" component="p">
+                                                {ProjectItems[2].description}
+                                            </Typography>
+                                        </CardContent>
+                                    </CardActionArea>
+                                    <CardActions>
+                                        <Button variant="contained" size="small" color="secondary">
+                                            Github
+                                        </Button>
+                                        <Button variant="contained" size="small" color="secondary">
+                                            Documentation
+                                        </Button>
+                                    </CardActions>
+                                </Card>
+                                {/*card ends here*/}
+                            </Grid>
+                            <Grid item xs={12} sm={6} lg={4}>
+                                {/*insert card here*/}
+
+                                <Card className={classes.root} style={{backgroundColor: "#ffff"}} variant="outlined">
+                                    <CardActionArea>
+                                        <CardMedia
+                                            className={classes.media}
+                                            image= {ProjectItems[3].imageUrl}
+                                            title= {ProjectItems[3].title}
+                                        />
+                                        <CardContent>
+                                            <Typography style={{color: "#ff006e", fontFamily:'Poppins', fontWeight: 'bold'}} className="title" gutterBottom variant="h5" component="h2">
+                                                {ProjectItems[3].title}
+                                            </Typography>
+                                            <Typography style={{color: "black", fontFamily:'Poppins'}} variant="body2" color="textSecondary" component="p">
+                                                {ProjectItems[3].description}
+                                            </Typography>
+                                        </CardContent>
+                                    </CardActionArea>
+                                    <CardActions>
+                                        <Button variant="contained" size="small" color="secondary">
+                                            Github
+                                        </Button>
+                                        <Button variant="contained" size="small" color="secondary">
+                                            Documentation
+                                        </Button>
+                                    </CardActions>
+                                </Card>
+                                {/*card ends here*/}
+                            </Grid>
                         </Grid>
                     </div>
                 </Container>
