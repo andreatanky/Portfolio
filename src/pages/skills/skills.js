122697bb080a58654a279f42c77c7acaa30dabe4
Andrea Tan <andreaxtan611@gmail.com>
2021-01-08 01:12:20 +0800

    Add some icons

--- src/pages/skills/skills.js
+++ src/pages/skills/skills.js
@@ -8,8 +8,12 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import {Container, withStyles} from "@material-ui/core";
+import {Avatar, Container, withStyles} from "@material-ui/core";
 import Box from '@material-ui/core/Box';
+import python from '../../assets/skills_imgs/python.png';
+import js from '../../assets/skills_imgs/js.png';
+import java from '../../assets/skills_imgs/java.png';
+import c from '../../assets/skills_imgs/C.png';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -40,14 +44,22 @@ function Skills() {
                 <h1 >Skills</h1>
                 <Grid className="area" container spacing={1}>
                     <Grid item xs={12} sm={12} lg={12}>
-                        <Box width={1} height="99%" style={{backgroundColor: "#003459", borderRadius: '25px'}}>
+                        <Box id="box" width={1} height="99%" style={{backgroundColor: "#003459", borderRadius: '25px'}}>
                             <Box width="18%" height="100%" style={{backgroundColor: "#ce4257", borderRadius: '25px'}}>
                                 <p className="type">Back-end</p>
+                            </Box>
+                            <Box width="18%" height="100%">
+                                <div id="logos">
+                                    <p id="para1">
+                                        <Avatar variant="square" src={python}/></p>
+                                    <p id="para"> <Avatar variant="square" src={js}/></p>
+                                    <p id="para"> <Avatar variant="square" src={java}/></p>
+                                </div>
                             </Box>
                         </Box>
                     </Grid>
                     <Grid item xs={12} sm={12} lg={12}>
-                        <Box width={1} height="99%" style={{backgroundColor: "#003459", borderRadius: '25px'}}>
+                        <Box id="box" width={1} height="99%" style={{backgroundColor: "#003459", borderRadius: '25px'}}>
                             <Box width="18%" height="100%" style={{backgroundColor: "#ce4257", borderRadius: '25px'}}>
                                 <p className="type">Front-end</p>
                             </Box>
@@ -62,28 +74,28 @@ function Skills() {
 
                         {/*    </CardContent>*/}
                         {/*</Card>*/}
-                        <Box width={1} height="99%" style={{backgroundColor: "#003459", borderRadius: '25px'}}>
+                        <Box id="box" width={1} height="99%" style={{backgroundColor: "#003459", borderRadius: '25px'}}>
                             <Box width="18%" height="100%" style={{backgroundColor: "#ce4257", borderRadius: '25px'}}>
                                 <p className="type">Languages</p>
                             </Box>
                         </Box>
                     </Grid>
                     <Grid item xs={12} sm={12} lg={12}>
-                        <Box width={1} height="99%" style={{backgroundColor: "#003459", borderRadius: '25px'}}>
+                        <Box id="box" width={1} height="99%" style={{backgroundColor: "#003459", borderRadius: '25px'}}>
                             <Box width="18%" height="100%" style={{backgroundColor: "#ce4257", borderRadius: '25px'}}>
                                 <p className="type">Databases</p>
                             </Box>
                         </Box>
                     </Grid>
                     <Grid item xs={12} sm={12} lg={12}>
-                        <Box width={1} height="90%" style={{backgroundColor: "#003459", borderRadius: '25px'}}>
+                        <Box id="box" width={1} height="90%" style={{backgroundColor: "#003459", borderRadius: '25px'}}>
                             <Box width="18%" height="100%" style={{backgroundColor: "#ce4257", borderRadius: '25px'}}>
                                 <p className="type">Version control</p>
                             </Box>
                         </Box>
                     </Grid>
                     <Grid item xs={12} sm={12} lg={12}>
-                        <Box width={1} height="90%" style={{backgroundColor: "#003459", borderRadius: '25px'}}>
+                        <Box id="box" width={1} height="90%" style={{backgroundColor: "#003459", borderRadius: '25px'}}>
                             <Box width="18%" height="100%" style={{backgroundColor: "#ce4257", borderRadius: '25px'}}>
                                 <p className="type">Hosting platforms</p>
                             </Box>
