cefb605c190e0d8dc14e420c4b65eacda7c67793
Andrea Tan <andreaxtan611@gmail.com>
2021-01-09 18:17:56 +0800

    Amend inner border radius

--- src/pages/skills/skills.js
+++ src/pages/skills/skills.js
@@ -14,7 +14,14 @@ import python from '../../assets/skills_imgs/python.png';
 import js from '../../assets/skills_imgs/js.png';
 import java from '../../assets/skills_imgs/java.png';
 import c from '../../assets/skills_imgs/C.png';
-import dart from '../../assets/skills_imgs/dart.png';
+import dart from '../../assets/skills_imgs/dart.jpg';
+import html from '../../assets/skills_imgs/html.png';
+import css from '../../assets/skills_imgs/css.png';
+import mdb from '../../assets/skills_imgs/mdb.png';
+import react from '../../assets/skills_imgs/react.png';
+import flutter from '../../assets/skills_imgs/flutter.png';
+import fb from '../../assets/skills_imgs/fb.png';
+import bs from '../../assets/skills_imgs/bs.png';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -46,34 +53,32 @@ function Skills() {
                 <Grid className="area" container spacing={1}>
                     <Grid item xs={12} sm={12} lg={12}>
                         <Box id="box" width={1} height="99%" style={{backgroundColor: "#003459", borderRadius: '25px'}}>
-                            <Box width="18%" height="100%" style={{backgroundColor: "#ce4257", borderRadius: '25px'}}>
-                                <p style={{textAlign: 'center', paddingTop: '0.9em'}} className="type">Languages</p>
+                            <Box width="18%" height="100%" style={{backgroundColor: "#ce4257", borderRadius: '25px 0px 0px 25px'}}>
+                                <p style={{textAlign: 'center', lineHeight:'100px'}} className="type">Languages</p>
                             </Box>
                             <Box width="18%" height="100%">
                                 <div id="logos">
-                                    <p id="para1">
-                                        <Avatar style={{ height: '45px', width: '45px', textAlign: 'center' }} variant="square" src={python}/></p>
-                                    <p id="para"> <Avatar style={{ height: '45px', width: '45px', textAlign: 'center' }} variant="square" src={js}/></p>
-                                    <p id="para"> <Avatar style={{ height: '45px', width: '45px', textAlign: 'center' }} variant="square" src={java}/></p>
-                                    <p id="para"> <Avatar style={{ height: '45px', width: '45px', textAlign: 'center' }} variant="square" src={c}/></p>
-                                    <p id="para"> <Avatar style={{ height: '45px', width: '45px', textAlign: 'center' }} variant="square" src={dart}/></p>
+                                    <p id="para1"><Avatar style={{ height: '45px', width: '45px', textAlign: 'center' }} variant="square" src={python}/><p style={{fontSize: '0.6em', color: '#ffffff'}}>Python</p></p>
+                                    <p id="para"> <Avatar style={{ height: '45px', width: '45px', textAlign: 'center' }} variant="square" src={js}/><p style={{fontSize: '0.6em', color: '#ffffff'}}>JavaScript</p></p>
+                                    <p id="para"> <Avatar style={{ height: '45px', width: '45px', textAlign: 'center' }} variant="square" src={java}/><p style={{fontSize: '0.6em', color: '#ffffff'}}>Java</p></p>
+                                    <p id="para"> <Avatar style={{ height: '45px', width: '45px', textAlign: 'center' }} variant="square" src={c}/><p style={{fontSize: '0.6em', color: '#ffffff'}}>C</p></p>
+                                    <p id="para"> <Avatar style={{ height: '45px', width: '45px', textAlign: 'center' }} variant="square" src={dart}/><p style={{fontSize: '0.6em', color: '#ffffff'}}>Dart</p></p>
                                 </div>
                             </Box>
                         </Box>
                     </Grid>
                     <Grid item xs={12} sm={12} lg={12}>
                         <Box id="box" width={1} height="99%" style={{backgroundColor: "#003459", borderRadius: '25px'}}>
-                            <Box width="18%" height="100%" style={{backgroundColor: "#ce4257", borderRadius: '25px'}}>
-                                <p style={{textAlign: 'center', paddingTop: '0.9em'}} className="type">Front-end</p>
+                            <Box width="18%" height="100%" style={{backgroundColor: "#ce4257", borderRadius: '25px 0px 0px 25px'}}>
+                                <p style={{textAlign: 'center', lineHeight:'100px'}} className="type">Front-end</p>
                             </Box>
                             <Box width="18%" height="100%">
                                 <div id="logos">
-                                    <p id="para1">
-                                        <Avatar style={{ height: '45px', width: '45px', textAlign: 'center' }} variant="square" src={python}/></p>
-                                    <p id="para"> <Avatar style={{ height: '45px', width: '45px', textAlign: 'center' }} variant="square" src={js}/></p>
-                                    <p id="para"> <Avatar style={{ height: '45px', width: '45px', textAlign: 'center' }} variant="square" src={java}/></p>
-                                    <p id="para"> <Avatar style={{ height: '45px', width: '45px', textAlign: 'center' }} variant="square" src={c}/></p>
-                                    <p id="para"> <Avatar style={{ height: '45px', width: '45px', textAlign: 'center' }} variant="square" src={dart}/></p>
+                                    <p id="para1"><Avatar style={{ height: '45px', width: '45px', textAlign: 'center' }} variant="square" src={html}/><p style={{fontSize: '0.6em', color: '#ffffff'}}>HTML5</p></p>
+                                    <p id="para"> <Avatar style={{ height: '45px', width: '45px', textAlign: 'center' }} variant="square" src={css}/><p style={{fontSize: '0.6em', color: '#ffffff'}}>CSS3</p></p>
+                                    <p id="para"> <Avatar style={{ height: '45px', width: '45px', textAlign: 'center' }} variant="square" src={bs}/><p style={{fontSize: '0.6em', color: '#ffffff'}}>Bootstrap</p></p>
+                                    <p id="para"> <Avatar style={{ height: '45px', width: '45px', textAlign: 'center' }} variant="square" src={flutter}/><p style={{fontSize: '0.6em', color: '#ffffff'}}>Flutter</p></p>
+                                    <p id="para"> <Avatar style={{ height: '45px', width: '45px', textAlign: 'center' }} variant="square" src={react}/><p style={{fontSize: '0.6em', color: '#ffffff'}}>React</p></p>
                                 </div>
                             </Box>
                         </Box>
@@ -88,8 +93,8 @@ function Skills() {
                         {/*    </CardContent>*/}
                         {/*</Card>*/}
                         <Box id="box" width={1} height="99%" style={{backgroundColor: "#003459", borderRadius: '25px'}}>
-                            <Box width="18%" height="100%" style={{backgroundColor: "#ce4257", borderRadius: '25px'}}>
-                                <p style={{textAlign: 'center', paddingTop: '0.9em'}} className="type">Back-end</p>
+                            <Box width="18%" height="100%" style={{backgroundColor: "#ce4257", borderRadius: '25px 0px 0px 25px'}}>
+                                <p style={{textAlign: 'center', lineHeight:'100px'}} className="type">Back-end</p>
                             </Box>
                             <Box width="18%" height="100%">
                                 <div id="logos">
@@ -105,8 +110,8 @@ function Skills() {
                     </Grid>
                     <Grid item xs={12} sm={12} lg={12}>
                         <Box id="box" width={1} height="99%" style={{backgroundColor: "#003459", borderRadius: '25px'}}>
-                            <Box width="18%" height="100%" style={{backgroundColor: "#ce4257", borderRadius: '25px'}}>
-                                <p style={{textAlign: 'center', paddingTop: '0.9em'}} className="type">Databases</p>
+                            <Box width="18%" height="100%" style={{backgroundColor: "#ce4257", borderRadius: '25px 0px 0px 25px'}}>
+                                <p style={{textAlign: 'center', lineHeight:'100px'}} className="type">Databases</p>
                             </Box>
                             <Box width="18%" height="100%">
                                 <div id="logos">
@@ -122,8 +127,8 @@ function Skills() {
                     </Grid>
                     <Grid item xs={12} sm={12} lg={12}>
                         <Box id="box" width={1} height="90%" style={{backgroundColor: "#003459", borderRadius: '25px'}}>
-                            <Box width="18%" height="100%" style={{backgroundColor: "#ce4257", borderRadius: '25px'}}>
-                                <p style={{textAlign: 'center', paddingTop: '0.6em'}} className="type">VCS</p>
+                            <Box width="18%" height="100%" style={{backgroundColor: "#ce4257", borderRadius: '25px 0px 0px 25px'}}>
+                                <p style={{textAlign: 'center'}} className="type">VCS</p>
                             </Box>
                             <Box width="18%" height="100%">
                                 <div id="logos">
@@ -139,7 +144,7 @@ function Skills() {
                     </Grid>
                     <Grid item xs={12} sm={12} lg={12}>
                         <Box id="box" width={1} height="90%" style={{backgroundColor: "#003459", borderRadius: '25px'}}>
-                            <Box width="18%" height="100%" style={{backgroundColor: "#ce4257", borderRadius: '25px'}}>
+                            <Box width="18%" height="100%" style={{backgroundColor: "#ce4257", borderRadius: '25px 0px 0px 25px'}}>
                                 <p className="type">Hosting platforms</p>
                             </Box>
                             <Box width="18%" height="100%">
