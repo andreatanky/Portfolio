6be287bb9448aa2939356e706aa7f685198dfe87
Andrea Tan <andreaxtan611@gmail.com>
2021-01-11 20:05:08 +0800

    Update colours of background

--- src/pages/contact/triangle.js
+++ src/pages/contact/triangle.js
@@ -4,7 +4,7 @@ const Triangle = () => {
     return (
         <div className="triangle">
             <svg xmlns="http://www.w3.org/2000/svg" height="75" width="100%" version="1.1" viewBox="0 0 100 102" preserveAspectRatio="none">
-                    <path d="M0 0 L50 100 L100 0 Z" fill="#77A0B9" stroke="#77A0B9"></path>
+                    <path d="M0 0 L50 100 L100 0 Z" fill="#8896B0" stroke="#8896B0"></path>
             </svg>
         </div>
     );
--- src/pages/experience/timelineElements.js
+++ src/pages/experience/timelineElements.js
@@ -3,24 +3,24 @@ let timelineElements = [
         id: 1,
         title: "Orbital 2020",
         subtitle: "Independent Software Development Project",
-        description: "Developed a mobile application with Flutter and Dart for front-end and Firebase/Google Cloud Firestore with back-end that is compatible on both IOS and Android",
+        description: "Developed a cross-platform mobile application with Flutter and Dart for front-end and Firebase/Google Cloud Firestore for back-end.\n Attained highest level of achievement, Artemis.",
         date: "May 2020 - Aug 2020",
         icon: "school"
     },
     {
         id: 2,
-        title: "Hack and Roll 2020",
+        title: "Hack and Roll 2021",
         subtitle: "Hackathon by NUSHackers",
-        description: "description 1",
+        description: "Participated in this 24 hour hackathon and developed a mobile application with a function that allows users to set alarms with customisable snooze timings.",
         date: "Jan 2021 - Jan 2021",
         icon: "school"
     },
     {
         id: 3,
-        title: "title 1",
-        subtitle: "intern at..",
-        description: "description 1",
-        date: "Aug 2016 - present",
+        title: "I am currently looking for an internship! :(",
+        subtitle: "",
+        description: "If you would like to offer me an interview, please view the contact me page. Thank you!",
+        date: "Actively searching for internships",
         icon: "internship"
     }
 ];
